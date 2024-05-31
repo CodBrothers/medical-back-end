2e247d72123e2ad0ece952a2424b57db08885f3b
CodBrothers <codeerssr@gamil.com>
2024-06-01 00:09:32 +0530

    added cors package

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const authRoutes = require('./src/Routes/authRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT;
 console.log(process.env.PORT);
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
-
+app.use(cors());
 // Use auth routes
 app.use('/auth', authRoutes);
 
